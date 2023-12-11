46c8eccd52047f8ea2f7faf8ca288970a7f3da56
Kelvin Chan <kechan@gmail.com>
2023-12-11 11:56:01 -0500

    locallogic rewrite agent

--- realestate_spam/llm/chatgpt.py
+++ realestate_spam/llm/chatgpt.py
@@ -1,7 +1,8 @@
 from typing import List, Dict
 from raa import RAA    
-
-import json
+from tenacity import RetryError
+from openai.error import OpenAIError, APIConnectionError
+import json, re
 
 from ..utils.misc import num_tokens_from_string, num_tokens_from_messages
 
@@ -244,7 +245,243 @@ each other as message_0 is from message_1.
     return "<inputs>\n" + '\n'.join([f"<input>{text}</input>" for text in inputs]) + "\n</inputs>\n"
     
 
+class LocalLogicGPTRewriter:
+  def __init__(self, llm_model: str, available_sections = ['housing', 'transport', 'services', 'character'], property_type: str = None):
+    self.llm_model = llm_model
+    self.available_sections = available_sections.copy()  
+    self._sections = available_sections.copy()
+    self.property_type = property_type
 
+    self.property_pluralized = None
+    if self.property_type is not None:
+      if self.property_type.lower() == 'condo':
+        self.property_pluralized = "Condos"
+      elif self.property_type.lower() == 'townhouse':
+        self.property_pluralized = "Townhouses"
+      elif self.property_type.lower() == 'semi-detached':
+        self.property_pluralized = "Semi-detached Homes"
+      elif self.property_type.lower() == 'luxury':
+        self.property_pluralized = "Luxury Homes"
+      elif self.property_type.lower() == 'investment':
+        self.property_pluralized = "Investment Properties"
+      else:
+        self.property_pluralized = f"{self.property_type.capitalize()} Homes"
+
+    self._construct_sys_prompt()
+
+    # chatgpt completion API wrapped up as an agent
+    self.raa = RAA(
+      llm_model=self.llm_model, 
+      sys_prompt=self.system_prompt, 
+      # api_key_env_var='FAKE_JUMPTOOLS_OPENAI_API_KEY'
+      api_key_env_var='JUMPTOOLS_OPENAI_API_KEY'
+    )    
+
+  @property
+  def sections(self):
+    return self._sections
+
+  @sections.setter
+  def sections(self, value):
+    self._sections = value
+    self._construct_sys_prompt()
+
+  def test_openai_health(self) -> bool:
+    return self.raa.test_health()
+
+  def rewrite(self, params_dict: Dict[str, str] = None, use_rag=True, **kwargs) -> Dict[str, str]:
+    """
+    params_dict: contains info for RAG, numeric information to be injected into user prompt. Use it if provided.
+    """
+    error_message = ''
+    prediction_str = None
+
+    messages = self.construct_openai_user_prompt(params_dict=params_dict, use_rag=use_rag, **kwargs)
+    try:
+      prediction_str = self.raa.get_completion_from_messages(messages, temperature=1.0, max_tokens=1000, debug=False)
+      # parse housing, transport, services, character from prediction_str
+      # simulate invalid prediction_str 
+      # prediction_str = "corrupted junk"
+      # prediction_str = "<housing>This area is really awesome to live in.</housing>"
+      parsed_response = self.parse_response(prediction_str)
+
+      # rewrite is considered successful only if all sections are being rewritten in the required format processed in parse_response,
+      # so check this here.
+      success = True
+      for section in self._sections:
+        rewrite = parsed_response.get(section, None)
+        if rewrite is None or len(rewrite) < 5:    # TODO: just some text for now.
+          success = False
+          break
+
+      if not success:
+        error_message += f'Not all sections are rewritten. Please try again. '
+
+    except RetryError as e:
+      # print(f'RetryError caught in LocalLogicGPTRewriter.rewrite(): {e}')
+      
+      last_attempt = e.last_attempt   # need to unwrap the exceptions
+      # print(type(last_attempt.exception()))
+
+      if last_attempt:
+        try:
+          last_attempt.result()
+        except OpenAIError as oe:
+          error_message += str(oe)
+          if hasattr(oe, 'error'):
+            error_message += f" OpenAI Error: {oe.error.get('message', None)}" 
+          if hasattr(oe, 'chat_messages'):
+            error_message += f" Chat messages: {oe.chat_messages}"
+        except Exception as e:
+          print(f'caught a generic exception: {e}')
+          error_message += str(e)
+    
+    except Exception as e:      
+      error_message += str(e)
+      print(f'Exception caught in LocalLogicGPTRewriter.rewrite(): {error_message}')
+    finally: 
+      # return the dict that will contain the consolidated error messages
+      if error_message != '':
+        return {**{key: None for key in kwargs.keys()}, "error_message": error_message}
+      else:
+        parsed_response['error_message'] = None
+        return parsed_response
+
+
+  def _construct_sys_prompt(self):
+    tag_for_sections = ', '.join([f"<{section}></{section}>" for section in self._sections])
+    are_or_is, subject_verb, plural_section = ("are", "They are", "sections") if len(self._sections) > 1 else ("is", "It is", "section")
+
+    self.system_prompt = f"""
+You are locallogic writer with lot of experience working with neighborhood profiles. 
+Please rewrite the below with interesting variations keeping the tone and style similar. 
+There {are_or_is} {len(self._sections)} {plural_section} and {subject_verb.lower()} delimited by {tag_for_sections}.
+"""
+
+  def construct_openai_user_prompt(self, params_dict: Dict[str, str] = None, use_rag=True, **kwargs) -> List[Dict]:
+    """
+    Construct messages to send to openai
+    """
+    if self.property_type is not None:
+      assert params_dict is not None, "if rewrite is property specific, at least one dynamic param must be provided."
+
+    # Figure out what sections are provided and set sections to them 
+    # (which consequentially update the sys prompt as appropriate).
+    self.sections = [section for section in self.available_sections if kwargs.get(section, None) is not None]
+
+    if use_rag:
+      rag_string = None
+      if params_dict is not None:
+        params_dict_str = '\n  '.join([f"{k+1}) {key}: {value}" for k, (key, value) in enumerate(params_dict.items()) if value is not None])
+        if self.property_type is None:
+          rag_string = f"""For housing section only if present, please incorporate these data points:
+
+  {params_dict_str}
+
+"""
+        else:
+          rag_string = f"""For housing section only if present, rewrite text customized for {self.property_pluralized} in the city referenced
+while utilizing these data points:
+
+  {params_dict_str}
+
+"""
+          
+    else:  # placeholder way
+      placeholder_string = "Anything bracketed by [] are placeholders to be determined, please retain them without alteration.\n"
+      if params_dict is not None:
+        param_keys = list(params_dict.keys())
+        # params_dict_str = '\n'.join(param_keys)
+        params_dict_str = '\n  '.join([f"{k+1}) {key} which is {value}" for k, (key, value) in enumerate(params_dict.items()) if value is not None])
+        if self.property_type is None:
+          placeholder_string += f"""For housing section only if present, please incorporate these infos using the following placeholders:
+  
+  {params_dict_str}
+
+"""
+        else:
+          placeholder_string += f"""For housing section only, rewrite text customized exclusively for {self.property_pluralized} in the city referenced. 
+For any stats and numeric info mentioned in the original text, provide a version specific to {self.property_pluralized} using these placeholders: 
+
+  {params_dict_str} 
+
+For stats and numeric info that are agnostic to property type {self.property_type.lower()}, please retain.
+"""
+
+    section_strings = []
+    for section in self._sections:
+      content = kwargs.get(section, None)
+      section_string = f"<{section}>{content}</{section}>" if content is not None else "" #f"<{section}>None</{section}>"
+      section_strings.append(section_string)
+
+    # Add guidelines
+    if self.property_type is None:
+      guidelines = f"""
+
+IMPORTANT
+
+0. Wrap each section in its own tag, e.g. <housing></housing>
+1. For housing if present, always start with 'Homes for sale in {{whatever city}}'.
+2. output something with roughly the same # of words each. 
+3. Verbiage may not be too "flowery"
+4. Be sure to retain numerical and highway or major street info.
+"""      
+    else:    
+      guidelines = f"""
+
+IMPORTANT
+
+0. Wrap each section in its own tag, e.g. <housing></housing>
+1. For housing if present, always start with '{self.property_pluralized} for sale in {{whatever city}}', and be sure to retain {self.property_type.lower()} agnostic info without referring to {self.property_type.lower()}.
+2. output something with roughly the same # of words each. 
+3. Verbiage may not be too "flowery"
+4. Be sure to retain numerical and highway or major street info.
+"""      
+
+    if use_rag:
+      user_prompt = (rag_string if rag_string is not None else "") + "\n".join(section_strings) + guidelines
+    else:
+      user_prompt = placeholder_string + "\n".join(section_strings) + guidelines
+
+    messages = [{"role": "user", "content": user_prompt}]
+    return messages
+    
+    
+  def print_prompt(self, **kwargs) -> str:
+    messages = self.construct_openai_user_prompt(**kwargs)
+    user_prompt = messages[-1]['content']   # get the user role message content
+
+    return self.system_prompt + "\n" + user_prompt + "\n"
+  
+  def parse_response(self, prediction_str: str) -> Dict[str, str]:
+    """
+    Parse prediction_str into housing, transport, services, character
+    """
+
+    parsed_response = {}
+
+    for section in self._sections:
+      # Construct regex pattern for each section
+      pattern = re.compile(f'<{section}>(.*?)</{section}>', re.DOTALL)
+
+      # Search for the pattern in the input_text
+      match = pattern.search(prediction_str)
+      
+      if match:
+        # If a match is found, store the content in the parsed_response dictionary
+        parsed_response[section] = match.group(1).strip()
+      else:
+        # If no match is found, store None in the parsed_data dictionary
+        parsed_response[section] = None
+
+    return parsed_response
+
+
+
+
+
+
+    
     
 
 
